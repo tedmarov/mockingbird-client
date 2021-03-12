0523beaa32ecc183d1ab6fe57debddc613e1ed39
ted <tedmarov@gmail.com>
2021-03-12 11:19:04 -0600

    Install and import, send help

--- src/components/voice/VoiceForm.js
+++ src/components/voice/VoiceForm.js
@@ -1,3 +1,4 @@
 // Should be used to capture simple things; Get it to work with React JS
 // Have something that proves whichever NPM trying to use
 // Get this piece working first
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
