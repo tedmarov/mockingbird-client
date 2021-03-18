0881aee2f14487904edcdbfefbebe4444128d6d9
ted <tedmarov@gmail.com>
2021-03-18 13:07:48 -0500

    Refactored from Rare

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,58 +1,63 @@
 import React, { useRef } from "react"
-import { Link } from "react-router-dom";
-import { useHistory } from "react-router-dom"
-import "./Login.css"
+import { Link, useHistory } from "react-router-dom"
+import "./Auth.css"
 
 
-export const Login = props => {
+export const Login = () => {
     const email = useRef()
     const password = useRef()
-    const existDialog = useRef()
+    const invalidDialog = useRef()
     const history = useHistory()
-
-    const existingUserCheck = () => {
-        return fetch(`http://localhost:8088/customers?email=${email.current.value}`)
-            .then(res => res.json())
-            .then(user => user.length ? user[0] : false)
-    }
 
     const handleLogin = (e) => {
         e.preventDefault()
 
-        existingUserCheck()
-            .then(exists => {
-                if (exists) {
-                    localStorage.setItem("kennel_customer", exists.id)
+        return fetch("http://127.0.0.1:8000/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                username: email.current.value,
+                password: password.current.value
+            })
+        })
+            .then(res => res.json())
+            .then(res => {
+                if ("valid" in res && res.valid) {
+                    localStorage.setItem("birdie", res.token )
+                    // localStorage.setItem("admin", res.staff)
                     history.push("/")
-                } else {
-                    existDialog.current.showModal()
+                }
+                else {
+                    invalidDialog.current.showModal()
                 }
             })
     }
 
     return (
         <main className="container--login">
-            <dialog className="dialog dialog--auth" ref={existDialog}>
-                <div>User does not exist</div>
-                <button className="button--close" onClick={e => existDialog.current.close()}>Close</button>
+            <dialog className="dialog dialog--auth" ref={invalidDialog}>
+                <div>Email or password was not valid.</div>
+                <button className="button--close" onClick={e => invalidDialog.current.close()}>Close</button>
             </dialog>
-
             <section>
                 <form className="form--login" onSubmit={handleLogin}>
-                    <h1>Nashville Kennels</h1>
+                    <h1>Level Up</h1>
                     <h2>Please sign in</h2>
                     <fieldset>
                         <label htmlFor="inputEmail"> Email address </label>
-                        <input ref={email} type="email"
-                            id="email"
-                            className="form-control"
-                            placeholder="Email address"
-                            required autoFocus />
+                        <input ref={email} type="email" id="email" className="form-control" defaultValue="me@me.com" placeholder="Email address" required autoFocus />
                     </fieldset>
                     <fieldset>
-                        <button type="submit">
-                            Sign in
-                        </button>
+                        <label htmlFor="inputPassword"> Password </label>
+                        <input ref={password} type="password" id="password" className="form-control" defaultValue="me" placeholder="Password" required />
+                    </fieldset>
+                    <fieldset style={{
+                        textAlign:"center"
+                    }}>
+                        <button className="btn btn-1 btn-sep icon-send" type="submit">Sign In</button>
                     </fieldset>
                 </form>
             </section>
@@ -62,4 +67,3 @@ export const Login = props => {
         </main>
     )
 }
-
