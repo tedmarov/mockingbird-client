ceedf04602cff9a3d5fe26ff95b00772868f9658
ted <tedmarov@gmail.com>
2021-03-22 14:54:44 -0500

    More progress on Voice Form

--- src/components/voice/VoiceForm.js
+++ src/components/voice/VoiceForm.js
@@ -3,8 +3,7 @@
 // Use this form as a landing spot for the first incarnation of Mockingbird
 // Form first, then store the voice, then have a separate onClick/Submit event for the voice
 
-import React, { useEffect, useState } from "react"
-import { Route, Redirect } from "react-router-dom"
+import React, { useCallback, useContext, useEffect, useState } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 import { VoiceContext } from "./VoiceProvider.js"
@@ -15,13 +14,52 @@ import { CategoryContext } from "../category/CategoryProvider.js"
 
 
 export const VoiceForm = (props) => {
-    []
+    const { voices, addVoice, updateVoice, getVoices, deleteVoice } = useContext(VoiceContext)
+    const { categories, getCategories } = useContext(CategoryContext)
     const { transcript, resetTranscript } = useSpeechRecognition()
     
+    // Component state
+    const [voice, setVoice] = useState(
+        {
+            categoryId: "0"
+        }
+    )
+
+    // Something of a URL parameter
+    const editMode = props.match.params.hasOwnProperty("voiceId")
+
+    const handleControlledInputChange = (e) => {
+        /*
+            When changing a state object or array, always create a new one
+            and change state instead of modifying current one
+        */
+        const newVoice = Object.assign({}, voice)
+        newVoice[e.target.name] = e.target.value
+        setVoice(newVoice)
+    }
+
+    /*
+        If there is a URL parameter, then the user has chosen to
+        edit an event.
+            1. Get the value of the URL parameter.
+            2. Use that `id` to find the animal.
+            3. Update component state variable.
+    */
+
     useEffect(() => {
+        getVoices()
+        getTypes()
     }, [transcript])
 
     console.log(`${transcript}`)
+
+    const getVoiceInEditMode = () => {
+        if (editMode) {
+            const voiceId = +(props.match.params.voiceId)
+            const selectedVoice = voices.find(v => v.id === voiceId) || {}
+            setVoice(selectedVoice)
+        }
+    }
 
     // Sets the state of the empty values for a Voice
     const [voice, setVoice] = useState({})
@@ -44,6 +82,24 @@ export const VoiceForm = (props) => {
         setVoice(newVoice)
     }
 
+    useEffect(() => {
+        getVoiceInEditMode()
+    }, [voices]
+
+
+    const constructNewVoice = () => {
+        const categoryId = +(voice.categoryId)
+
+        if (categoryId === 0) {
+            window.alert("Please select a category.")
+        } else {
+            if (editMode) {
+                updateVoice({
+                    
+                })
+            }
+        }
+    }
 return (
     <div className="container">
         <div>
